d98c69072102ca9f933bfbbcedec3a3bf2bd4cf1
Rodolfo Win <rndcsjj@gmail.com>
2024-04-22 21:00:26 -0300

    feat: deleta comentario

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,7 +1,12 @@
 import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 import {ThumbsUp, Trash} from 'phosphor-react';
-export function Comment({content}) {
+export function Comment({content, onDeleteComment}) {
+
+  function handleDeleteComment() {
+    onDeleteComment(content)
+  }
+
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src='https://github.com/iTzRodz.png'/>
@@ -14,7 +19,7 @@ export function Comment({content}) {
               <time title='11 de Maio às 08:13' dateTime='2022-05-11 08:13:30'>Cerca de 1h atrás</time>
             </div>
 
-            <button title='Deletar Comentário'>
+            <button onClick={handleDeleteComment} title='Deletar Comentário'>
               <Trash size={24}/>
             </button>
           </header>
